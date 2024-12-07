9d659a4639cb0daefa5d3a2add7ca04c7dac6cb3
Rath Pascal <rath@oxl.at>
2024-12-07 17:54:47 +0100

    update docs-infra

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
 # pylint: disable=W0622
-_logo = 'https://raw.githubusercontent.com/ansibleguy/webui/latest/src/ansibleguy-webui/aw/static/img/logo.svg'
-
 project = 'AnsibleGuy WebUI'
 copyright = f'{datetime.now().year}, AnsibleGuy'
 author = 'AnsibleGuy'
@@ -11,9 +9,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 html_theme = 'piccolo_theme'
 html_static_path = ['_static']
-html_logo = _logo
-html_favicon = _logo
-html_css_files = ['css/main.css']
+html_logo = '_static/img/logo.svg'
+html_favicon = '_static/img/logo.svg'
+html_js_files = ['https://files.oxl.at/js/feedback.js']
+html_css_files = ['css/main.css', 'https://files.oxl.at/css/feedback.css']
 master_doc = 'index'
 display_version = True
 sticky_navigation = True
@@ -23,6 +22,7 @@ source_suffix = {
 }
 html_theme_options = {
     'banner_text': '<a href="https://github.com/ansibleguy/webui">Repository on GitHub</a> | '
-                   '<a href="https://github.com/ansibleguy/webui/issues/new/choose">Report errors</a>'
+                   '<a href="https://github.com/ansibleguy/webui/issues/new/choose">Report Errors</a> | '
+                   '<a href="https://www.o-x-l.com">Get Support</a>'
 }
 html_short_title = 'Ansible WebUI'
